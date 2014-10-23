bd657402b0a6bd7ff2477cfb5da5e183128667e1
valentinvs <valentin_vs@hotmail.com>
2014-10-23 20:56:14 +0300

    TheCompany: Initial Solution and projects commit

---
+++ TheCompany/TheCompany.Web.Frontend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheCompany.Web.Frontend.Startup))]
+namespace TheCompany.Web.Frontend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
