f228e970aaeb2a801019d00f5d7ce9eef575d913
valentinvs <valentin_vs@hotmail.com>
2014-11-01 17:35:41 +0200

    TheCompany: Fixed failing build due to not comitted files

---
+++ TheCompany/TheCompany.Web.Frontend/Controllers/CuisineController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheCompany.Web.Frontend.Controllers
+{
+    public class CuisineController : BaseController
+    {
+        //
+        // GET: /Kitchen/
+        public ActionResult Index()
+        {
+            var availbleMenus = TheCompanyDb.Menus.Where(m => m.Deleted == false);
+            return View(availbleMenus);
+        }
+
+        //
+        //// GET: /Kitchen/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// GET: /Kitchen/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// POST: /Kitchen/Create
+        //[HttpPost]
+        //public ActionResult Create(FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        ////
+        //// GET: /Kitchen/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// POST: /Kitchen/Edit/5
+        //[HttpPost]
+        //public ActionResult Edit(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        ////
+        //// GET: /Kitchen/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// POST: /Kitchen/Delete/5
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
